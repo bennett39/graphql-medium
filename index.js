6d43a0f357c50060ebe7fde71cc6b8d4eae5eb84
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-05-24 12:14:30 -0400

    Add route for '/'

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.listen(4000, function () {
+app.get('/', function(req, res) {
+  res.send('Express is working!')
+});
+
+app.listen(4000, function() {
   console.log('Listening on port 4000')
 });
