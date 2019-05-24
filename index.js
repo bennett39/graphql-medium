61eb5c3907061ef5e5a59169e8e0b646532b19be
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-05-24 12:47:34 -0400

    Add GraphQL

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express');
-const app = express();
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
+let schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+let root = {
+  hello: function() {
+    return 'Hello world!';
+  },
+};
+
+const app = express();
 app.get('/', function(req, res) {
   res.send('Express is working!')
 });
-
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 app.listen(4000, function() {
   console.log('Listening on port 4000')
 });
